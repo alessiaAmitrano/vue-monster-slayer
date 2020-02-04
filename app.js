8d9e51458e3a335518af303b2a5087dec3a76a2a
Alessia Amitrano <alessia.amitrano@imagination.com>
2020-02-04 21:36:34 +0000

    multi vue instances

--- app.js
+++ app.js
@@ -1,92 +1,34 @@
-new Vue({
-  el: '#app',
+var vue1 = new Vue({
+  el: '#app1',
   data: {
-    pHealth: 100,
-    mHealth: 100,
-    isGameOn: false,
-    turns: []
+    title: 'The VueJS Instance 1',
+    showParagraph: false
   },
   methods: {
-    startGame: function() {
-      this.isGameOn = true;
-      this.pHealth = 100;
-      this.mHealth = 100;
-      this.turns = [];
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
     },
-
-    attack: function() {
-      var damage = this.calcDamage(3, 10);
-      this.mHealth -= damage;
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player hits monster for ' + damage + 'pts!'
-      });
-      if (this.checkWin()) {
-        return;
-      }
-      this.monsterAttacks();
-    },
-
-    spAttack: function() {
-      var damage = this.calcDamage(10, 20);
-      this.mHealth -= damage;
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player hits monster HARD for ' + damage + 'pts!'
-      });
-      if (this.checkWin()) {
-        return;
-      }
-      this.monsterAttacks();
-    },
-
-    heal: function() {
-      if (this.pHealth <= 90) {
-        this.pHealth += 10;
-      } else {
-        this.pHealth = 100;
-      }
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player healed!'
-      });
-      this.monsterAttacks();
-    },
-
-    giveUp: function() {
-      this.isGameOn = false;
-    },
-
-    monsterAttacks: function() {
-      var damage = this.calcDamage(5, 12);
-      this.pHealth -= damage;
-      this.turns.unshift({
-        isPlayer: false,
-        text: 'Monster hits player for ' + damage + 'pts!'
-      });
-      this.checkWin();
-    },
-
-    calcDamage: function(min, max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min);
-    },
-
-    checkWin: function() {
-      if (this.mHealth <= 0) {
-        if (confirm('you Won! New Game?')) {
-          this.startGame();
-        } else {
-          this.isGameOn = false;
-        }
-        return;
-      } else if (this.pHealth <= 0) {
-        if (confirm('you Lost! New Game?')) {
-          this.startGame();
-        } else {
-          this.isGameOn = false;
-        }
-        return true;
-      }
+    updateTitle: function(title) {
+      this.title = title;
     }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+var vue2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The VueJS Instance 2',
+    showParagraph: false
   }
 });
